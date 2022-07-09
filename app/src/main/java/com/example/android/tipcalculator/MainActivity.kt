4a6570918f670d8418063804b2db3470cedd1a2b
cobey25 <Jacobikome@yahoo.com>
2022-07-09 10:21:34 -0400

    Upraged UI experience commit - Changed Launcher icon - Changed app color theme -Polished user experience using material components

--- app/src/main/java/com/example/android/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/android/tipcalculator/MainActivity.kt
@@ -1,14 +1,19 @@
 package com.example.android.tipcalculator
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 import com.example.android.tipcalculator.databinding.ActivityMainBinding
 import java.lang.NumberFormatException
 import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,21 +22,37 @@ class MainActivity : AppCompatActivity() {
 
         //set listener for when calculate button is clicked by user
         binding.calculateButton.setOnClickListener { calculateTip() }
+
+        binding.costOfServiceEditText.setOnKeyListener { view, keyCode, _ ->
+            handleKeyEvent(
+                    view,
+                    keyCode
+            )
+        }
     }
 
-    fun calculateTip() {
+    private fun calculateTip() {
 
         //take string input from user on edit text field and assign to a variable
-        val stringInTextField = binding.costOfService.text.toString()
+        val stringInTextField = binding.costOfServiceEditText.text.toString()
 
-        //convert string user input to double
-        val cost = stringInTextField.toDouble()
+        //convert string user input to double or null
+        val cost = stringInTextField.toDoubleOrNull()
 
-        //get the attribute of tip options and assign to a variable
-        val selectedId = binding.tipOptions.checkedRadioButtonId
+        //if no input is entered by the user, exit without executing the rest of the method
+        if (cost == null) {
+            Toast.makeText(
+                    this,
+                    "Error: Enter the cost of service to calculate tip",
+                    Toast.LENGTH_LONG
+            ).show()
+            binding.tipResult.text = ""
+            return
+        }
+
 
         //get tip percentage based on option selected
-        val tipPercentage = when(selectedId) {
+        val tipPercentage = when (binding.tipOptions.checkedRadioButtonId) {
             R.id.option_twenty_percent -> 0.20
             R.id.option_eighteen_percent -> 0.18
             else -> 0.15
@@ -41,13 +62,22 @@ class MainActivity : AppCompatActivity() {
         var tip = tipPercentage * cost
 
         //check if switch is on
-        val roundUp = binding.roundUpSwitch.isChecked
-
-        if (roundUp){
+        if (binding.roundUpSwitch.isChecked) {
             tip = kotlin.math.ceil(tip)
         }
 
         val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
         binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+    }
+
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            //hide keyboard
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
     }
 }
